94221a79d6fc3bc018f984f1d210ba7172b1954e
Rzrivas <rzrivas@aim.com>
2017-04-07 11:35:03 -0400

    First commit

---
+++ app/src/main/java/com/example/kevin/camera/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.kevin.camera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView result;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+        result = (ImageView)findViewById(R.id.imageView2);
+    }
+
+    public void onClickCapture(View view) {
+        Intent onClickCapture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (onClickCapture.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(onClickCapture, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            result.setImageBitmap(imageBitmap);
+        }
+    }
+
+}
